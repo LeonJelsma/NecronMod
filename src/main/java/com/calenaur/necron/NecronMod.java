5e820bad3cb76001d51f20eb45fbabca8f950626
Calenaur <timhoekstra2015@gmail.com>
2020-03-05 22:51:30 +0100

    Add NecronSoldier

--- src/main/java/com/calenaur/necron/NecronMod.java
+++ src/main/java/com/calenaur/necron/NecronMod.java
@@ -1,15 +1,25 @@
 package com.calenaur.necron;
 
+import com.calenaur.necron.entity.EntityNecronSoldier;
+import com.calenaur.necron.entity.type.EntityTypeNecronSoldier;
+import com.calenaur.necron.entity.type.EntityTypes;
 import com.calenaur.necron.item.ItemNecrodermisAxe;
 import com.calenaur.necron.item.ItemNecrodermisHoe;
 import com.calenaur.necron.item.ItemJeffPickaxe;
 import com.calenaur.necron.item.ItemNecrodermisPickaxe;
 import com.calenaur.necron.item.ItemNecrodermisShovel;
+import com.calenaur.necron.renderer.RendererNecronSoldier;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,6 +29,11 @@ public class NecronMod {
 
     public NecronMod() {
         MinecraftForge.EVENT_BUS.register(this);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+        RenderingRegistry.registerEntityRenderingHandler(EntityTypes.NECRON_SOLDIER, RendererNecronSoldier::new);
     }
 
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
@@ -30,6 +45,10 @@ public class NecronMod {
             event.getRegistry().register(new ItemNecrodermisHoe());
             event.getRegistry().register(new ItemNecrodermisPickaxe());
             event.getRegistry().register(new ItemNecrodermisShovel());
+        }
+        @SubscribeEvent
+        public static void onEntityTypeRegistry(final RegistryEvent.Register<EntityType<?>> event) {
+            event.getRegistry().register(new EntityTypeNecronSoldier());
         }
     }
 }
---
+++ src/main/java/com/calenaur/necron/entity/type/EntityTypes.java
@@ -0,0 +1,12 @@
+package com.calenaur.necron.entity.type;
+
+import com.calenaur.necron.entity.EntityNecronSoldier;
+import net.minecraft.entity.EntityType;
+import net.minecraftforge.registries.ObjectHolder;
+
+public class EntityTypes {
+
+	@ObjectHolder("necron:necron_soldier")
+	public static EntityType<EntityNecronSoldier> NECRON_SOLDIER;
+
+}
