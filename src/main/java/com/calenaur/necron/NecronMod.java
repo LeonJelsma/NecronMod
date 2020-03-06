7ea6d7d99a074e5489d05041d4d3b3ae0c5aa8bd
Calenaur <timhoekstra2015@gmail.com>
2020-03-06 02:11:36 +0100

    Add Regen Effect (#11)

--- src/main/java/com/calenaur/necron/NecronMod.java
+++ src/main/java/com/calenaur/necron/NecronMod.java
@@ -1,5 +1,6 @@
 package com.calenaur.necron;
 
+import com.calenaur.necron.event.EventNecrodermisItem;
 import com.calenaur.necron.registry.Registrar;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
@@ -9,6 +10,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class NecronMod {
     public NecronMod() {
         MinecraftForge.EVENT_BUS.register(new Registrar());
+        MinecraftForge.EVENT_BUS.register(new EventNecrodermisItem());
         FMLJavaModLoadingContext.get().getModEventBus().addListener(Registrar::clientSetup);
     }
 }
