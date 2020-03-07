c676f0b28d9921bf83e282c68b7695cfc36637ca
Calenaur <timhoekstra2015@gmail.com>
2020-03-07 02:18:33 +0100

    Fix hardcoded string

--- src/main/java/com/calenaur/necron/renderer/RendererNecronSoldier.java
+++ src/main/java/com/calenaur/necron/renderer/RendererNecronSoldier.java
@@ -1,5 +1,6 @@
 package com.calenaur.necron.renderer;
 
+import com.calenaur.necron.NecronMod;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.WitherSkeletonRenderer;
@@ -7,7 +8,7 @@ import net.minecraft.entity.monster.AbstractSkeletonEntity;
 import net.minecraft.util.ResourceLocation;
 
 public class RendererNecronSoldier extends WitherSkeletonRenderer {
-	private static final ResourceLocation NECRON_SOLDIER_TEXTURES = new ResourceLocation("necron","textures/entity/necron_soldier.png");
+	private static final ResourceLocation NECRON_SOLDIER_TEXTURES = new ResourceLocation(NecronMod.MOD_ID,"textures/entity/necron_soldier.png");
 
 	public RendererNecronSoldier(EntityRendererManager renderManagerIn) {
 		super(renderManagerIn);
