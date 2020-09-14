98124b8c0528ed00ada6494d6426d4433893c540
Leon <leonjelsma@gmail.com>
2020-09-14 15:59:59 +0200

    progress

--- src/main/java/com/calenaur/necron/block/BlockHungryMetalArranger.java
+++ src/main/java/com/calenaur/necron/block/BlockHungryMetalArranger.java
@@ -18,7 +18,7 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
@@ -77,7 +77,7 @@ public class BlockHungryMetalArranger extends Block {
     }
 
     public static Direction getFacingFromEntity(BlockPos clickedBlock, LivingEntity entity) {
-        Vec3d vec = entity.getPositionVec();
+        Vector3d vec = entity.getPositionVec();
         return Direction.getFacingFromVector((float) (vec.x - clickedBlock.getX()), (float) (vec.y - clickedBlock.getY()), (float) (vec.z - clickedBlock.getZ()));
     }
 
